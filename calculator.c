5acf306954a6d976e378e0d91c227b72d4b46505
tonnyogeto <tonnymaishaogeto@gmail.com>
2024-02-12 23:25:56 +0300

    debug 3

--- calculator.c
+++ calculator.c
@@ -1,38 +1,70 @@
-# include<stdio.h>
+// # include<stdio.h>
 
 
 
-int main(){
-    double num1,num2,result;
+// int main(){
+//     double num1,num2,result;
+//     char operator;
+
+//     printf("\nEnter an operator(+,-):");
+//     scanf("%c", &operator); 
+
+//     printf("Enter first number:");
+//     scanf("%lf", &num1); 
+
+    
+//     printf("Enter second number:");
+//     scanf("%lf", &num2); 
+
+//     switch(operator){
+//         case "+":
+//             result=num1+num2;
+//             printf("\nsum: %lf", result);
+//             break;
+
+//         case "-":
+//             result=num1-num2;
+//             printf("\ndiffence: %lf", result);
+//             break;
+
+//         default:
+//         printf("%c is not valid", operator); 
+//     }
+
+
+// }
+    
+
+
+#include <stdio.h>
+
+int main() {
+    double num1, num2, result;
     char operator;
 
-    printf("\nEnter an operator(+ -):");
-    scanf("%c", &operator); 
+    printf("\nEnter an operator(+,-):");
+    scanf(" %c", &operator); 
 
     printf("Enter first number:");
     scanf("%lf", &num1); 
 
-    
     printf("Enter second number:");
     scanf("%lf", &num2); 
 
-    switch(operator){
-        case "+":
-            result=num1+num2;
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
             printf("\nsum: %lf", result);
             break;
 
-        case "-":
-            result=num1-num2;
-            printf("\ndiffence: %lf", result);
+        case '-':
+            result = num1 - num2;
+            printf("\ndifference: %lf", result);
             break;
 
         default:
-        printf("%c is not valid", operator); 
+            printf("%c is not valid", operator); 
     }
 
-
+    return 0;
 }
-    
-
-
