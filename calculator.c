442698cda3c2e67f55c3031c9fa0c6144d2d06b5
tonnyogeto <tonnymaishaogeto@gmail.com>
2024-02-13 14:34:53 +0300

    5th commit

--- calculator.c
+++ calculator.c
@@ -1,39 +1,4 @@
 
-// #include <stdio.h>
-
-// int main() {
-//     double num1, num2, result;
-//     char operator;
-
-//     printf("\nEnter an operator(+,-):");
-//     scanf(" %c", &operator); 
-
-//     printf("Enter first number:");
-//     scanf("%lf", &num1); 
-
-//     printf("Enter second number:");
-//     scanf("%lf", &num2); 
-
-//     switch(operator) {
-//         case '+':
-//             result = num1 + num2;
-//             printf("\nsum: %lf", result);
-//             break;
-
-//         case '-':
-//             result = num1 - num2;
-//             printf("\ndifference: %lf", result);
-//             break;
-
-//         default:
-//             printf("%c is not valid", operator); 
-//     }
-
-//     return 0;
-// }
-
-
-
 #include <stdio.h>
 
 int main() {
