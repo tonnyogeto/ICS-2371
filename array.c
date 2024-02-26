78ebea799a5272b1d2cb9b43db21a938e1dd2594
tonnyogeto <tonnymaishaogeto@gmail.com>
2024-02-26 23:24:38 +0300

    second commit

--- array.c
+++ array.c
@@ -1,26 +1,11 @@
-// #include<stdio.h>
-
-// int main() {
-//     int a[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-//     int i;
-
-//     for (i = 0; i < 9; i++) {
-//         printf("%d ", a[i]);
-//     }
-
-
-
-//     return 0;
-// }
-
-
 #include<stdio.h>
 
 int main() {
     int a[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int i, middle;
+    int i, middle, temp;
 
-   
+    // Print original array
+    printf("Original array:");
     for (i = 0; i < 9; i++) {
         printf(" %d", a[i]);
     }
@@ -28,19 +13,37 @@ int main() {
     // Calculate the middle index
     middle = 9 / 2; // Assuming the array has odd number of elements
 
-    // Shift elements to the left starting from the middle index
+    // Save the middle element
+    temp = a[middle];
 
-
+    // Delete the middle element by shifting elements to the left
     for (i = middle; i < 8; i++) {
         a[i] = a[i + 1];
     }
 
-    // Print the updated array
+    // Print array after deletion
+    printf("\nArray after deletion:");
     for (i = 0; i < 8; i++) {
+        printf(" %d", a[i]);
+    }
+
+    // Shift elements to the right to make space for re-insertion
+    for (i = 8; i > middle; i--) {
+        a[i] = a[i - 1];
+    }
+
+    // Re-insert the middle element back into its original position
+    a[middle] = temp;
+
+    // Print updated array
+    printf("\nUpdated array:");
+    for (i = 0; i < 9; i++) {
         printf(" %d", a[i]);
     }
 
     return 0;
 }
+
+
 
 
